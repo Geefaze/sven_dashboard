5869e7665212c46ef6160eef512d49b7a3a2bdbf
Geefaze <geefaze.rap@gmail.com>
2026-07-08 09:42:36 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,15 +1,44 @@
 for match in matches:
 
-    with st.expander(
-        f"⚽ {match['home']} - {match['away']}"
-    ):
+   from api import get_today_matches
 
-        st.write(
-            f"""
-            🏆 Liga: {match['league']}
-            
-            🌍 Land: {match['country']}
-            
-            🕒 Zeit: {match['date']}
-            """
-        )
+
+st.subheader("📅 Tagesaktueller Match-Scanner")
+
+
+matches = get_today_matches()
+
+
+if len(matches) == 0:
+    st.warning("Keine Spiele für heute gefunden.")
+    st.stop()
+
+
+match_names = [
+    f"{m['home']} - {m['away']} ({m['league']})"
+    for m in matches
+]
+
+
+selected_match = st.selectbox(
+    "⚽ Spiel auswählen:",
+    match_names
+)
+
+
+selected_index = match_names.index(selected_match)
+
+selected_game = matches[selected_index]
+
+
+heim_team = selected_game["home"]
+
+auswaerts_team = selected_game["away"]
+
+
+game_input = f"{heim_team} - {auswaerts_team}"
+
+
+st.success(
+    f"Analyse geladen: {game_input}"
+)
